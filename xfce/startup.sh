57c7ca07fc8c735282a45bbc1949d55a7e82c85b
Marc <teste74@hotmail.fr>
2023-02-14 23:01:44 +0100

    Update startup.sh

--- xfce/startup.sh
+++ xfce/startup.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-####################################################################################
-# Configuration Environnement #
-###############################
-ENV DEBIAN_FRONTEND noninteractive
 
 ####################################################################################
 # Déclaration des variables d'environnement #
@@ -17,9 +13,9 @@ echo 'root:'${ROOT_PWD} | chpasswd;
 ####################################################################################
 # Configuration du SSH #
 ########################
-sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config 
-sed -ri "s/^Port\s+.*/Port 22/" /etc/ssh/sshd_config
-sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
+#sed -ri 's/^PermitRootLogin\s+.*/PermitRootLogin yes/' /etc/ssh/sshd_config 
+#sed -ri "s/^Port\s+.*/Port 22/" /etc/ssh/sshd_config
+#sed -ri 's/UsePAM yes/#UsePAM yes/g' /etc/ssh/sshd_config
 
 ####################################################################################
 # Lancemement de Supervisor #
